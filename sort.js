c5918c30563d7c56e1c37518956a56918cb2c1fc
RuFrontEnd <k718198@gmail.com>
2022-08-02 20:43:23 +0800

    新增 Selection Sort

--- sort.js
+++ sort.js
@@ -3,7 +3,6 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 const bubbleSort = (array) => {
-
   const sort = (array) => {
     const items = [...array];
 
@@ -33,4 +32,33 @@ const bubbleSort = (array) => {
   return sort(array);
 };
 
-console.log(bubbleSort(numbers));
+// console.log(bubbleSort(numbers));
+
+// Selection Sort
+
+const selectionSort = (nums) => {
+  const length = nums.length;
+
+  for (let i = 0; i < length; i++) {
+    let min = i, // 1
+      temp = nums[i]; // 99
+
+    for (let j = i + 1; j < length; j++) {
+      if (nums[j] < nums[min]) {
+        // 6 < 44
+        min = j; // 2
+      }
+    }
+
+    nums[i] = nums[min];
+    nums[min] = temp;
+  }
+
+  return nums;
+};
+
+// console.log(selectionSort(numbers));
+
+// Insertion Sort
+
+
