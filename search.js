9d64765e15871498b75226fc6b98c12859d7cb51
Luke <luke_xie@wistron.com>
2023-08-06 13:46:53 +0800

    binary search

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
 const util = require("util");
 const tree = require("./tree");
-// Linear Search
+// Linear Search (Sequenctial Search) O(n)
 
 let beasts = ["Centaur", "Godzilla", "Mosura", "Minotaur", "Hydra", "Nessie"];
 
@@ -12,7 +12,33 @@ let beasts = ["Centaur", "Godzilla", "Mosura", "Minotaur", "Hydra", "Nessie"];
 
 // console.log(beasts.includes("Godzilla"));
 
-// Binary Search
+// Binary Search O(LogN)
+const numbers = [1, 4, 6, 9, 12, 34, 45, 49]; // have been sorted
+// const numbers = [1, 4, 6, 9, 12, 38, 45, 49]; // none of target
+
+let minI = 0, // 4
+  maxI = numbers.length - 1,
+  found = false,
+  target = 34;
+
+while (!found) {
+  const middleI = Math.ceil((minI + maxI) / 2);
+
+  if (maxI === middleI || minI === middleI) break;
+
+  if (numbers[middleI] > target) {
+    maxI = middleI;
+  } else if (numbers[middleI] < target) {
+    minI = middleI;
+  } else if (numbers[middleI] === target) {
+    found = true;
+    break;
+  }
+}
+
+// console.log("found", found);
+
+// Travesal
 
 // Breath Fist Search (BFS)
 const numberTree = new tree.BinarySearchTree();
