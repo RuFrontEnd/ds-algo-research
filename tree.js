e777a7843f4bc4ff942d002c2cfa68e21222785d
Luke_Xie <k718198@gmail.com>
2023-10-21 18:07:59 +0800

    新增 BST insert

--- tree.js
+++ tree.js
@@ -16,9 +16,9 @@ const util = require("util");
 
 class Node {
   constructor(value) {
+    this.value = value;
     this.left = null;
     this.right = null;
-    this.value = value;
   }
 }
 const newNode = new Node(1); // { left: null, right: null, value: value }
@@ -37,108 +37,57 @@ class BinarySearchTree {
     this.root = null;
   }
 
-  insert(value) {
-    const newNode = new Node(value); // { left: null, right: null, value: value }
+  treeInsert(node) {
+    let y = null;
+    let x = this.root;
 
-    if (this.root === null) {
-      // 如果 tree 還沒有根節點
-      this.root = newNode; // 則新 node 成為根節點
+    while (x !== null) {
+      y = x
+      if (node.key < x.key) {
+        x = x.left
+      } else {
+        x = x.right
+      }
+    } // 讓 y 到達這個 tree 的最後一個 root
+
+    if (y === null) {
+      this.root = node
+    } else if (node.key < y.key) {
+      y.left = node
     } else {
-      let currentNode = this.root; // 有根節點, 則起始 node 為根節點
-      while (true) {
-        // left
-        if (value < currentNode.value) {
-          // 新值比當前節點值還小
-          if (!currentNode.left) { // 左邊已經沒有節點的話
-            currentNode.left = newNode; // 新增新節點至左邊
-            return this; // stop whie loop & 回傳整顆樹
-          }
-          currentNode = currentNode.left; // 當前節點變成前一節點的子左節點
-        }
-
-        // right
-        if (value >= currentNode.value) {
-          // 新值比當前節點值還大
-          if (!currentNode.right) { // 右邊已經沒有節點的話
-            currentNode.right = newNode; // 新增新節點至右邊
-            return this; // stop whie loop & 回傳整顆樹
-          }
-          currentNode = currentNode.right; // 當前節點變成前一節點的子右節點
-        }
-      }
+      y.right = node
     }
+
   }
-  lookup(value) {
-    if (!this.root) {
-      return false;
-    }
-    let currentNode = this.root;
 
-    while (currentNode) {
-      if (value < currentNode.value) {
-        currentNode = currentNode.left;
-      } else if (value > currentNode.value) {
-        currentNode = currentNode.right;
-      } else if (currentNode.value === value) {
-        return currentNode;
-      }
-    }
-    return false;
-  }
-  remove(value) {}
-
-  bfs() {
-    let currentNode = this.root;
-    let list = [];
-    let queue = [];
-    queue.push(currentNode);
-
-    while (queue.length > 0) {
-      currentNode = queue.shift();
-      list.push(currentNode.value);
-      if (currentNode.left) {
-        queue.push(currentNode.left);
-      }
-      if (currentNode.right) {
-        queue.push(currentNode.right);
-      }
-
-      // console.log(
-      //   util.inspect(
-      //     JSON.parse(JSON.stringify(queue)),
-      //     false,
-      //     null,
-      //     true /* enable colors */
-      //   )
-      // );
-    }
-
-    return list;
-  }
 }
 
-const tree = new BinarySearchTree();
-tree.insert(9);
-tree.insert(4);
-tree.insert(6);
-tree.insert(20);
-tree.insert(170);
-tree.insert(15);
-tree.insert(1);
+let bst = new BinarySearchTree();
+bst.treeInsert(new Node(1))
+bst.treeInsert(new Node(2))
+bst.treeInsert(new Node(3))
+bst.treeInsert(new Node(4))
+bst.treeInsert(new Node(5))
 
-// console.log(
-//   util.inspect(
-//     JSON.parse(JSON.stringify(tree)),
-//     false,
-//     null,
-//     true /* enable colors */
-//   )
-// );
+console.log(
+  util.inspect(
+    JSON.parse(JSON.stringify(bst)),
+    false,
+    null,
+    true /* enable colors */
+  )
+);
 
-console.log(tree.bfs());
+bst.treeInsert(new Node(5))
 
-// console.log("--- lookup ---\n", tree.lookup(20));
+console.log(
+  util.inspect(
+    JSON.parse(JSON.stringify(bst)),
+    false,
+    null,
+    true /* enable colors */
+  )
+);
 
-// Binary Heap =>
 
 module.exports = { BinarySearchTree };
