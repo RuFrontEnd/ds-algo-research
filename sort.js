42ca070d4028084e1b771ef4480e06261a267d87
Luke_Xie <k718198@gmail.com>
2025-06-17 20:51:22 +0800

    merge sort

--- sort.js
+++ sort.js
@@ -29,13 +29,14 @@ const selectionSort = (nums) => {
   const length = nums.length;
 
   for (let i = 0; i < length; i++) {
-    let min = i, // 1
-      temp = nums[i]; // 99
+    // n
+    let min = i,
+      temp = nums[i];
 
     for (let j = i + 1; j < length; j++) {
+      // n
       if (nums[j] < nums[min]) {
-        // 6 < 44
-        min = j; // 2
+        min = j;
       }
     }
 
@@ -55,44 +56,48 @@ const selectionSort = (nums) => {
 // Keeping inserting a new value into a sorted array.
 // Insert it to the correct spot so the sorted array remains sorted.
 const insertionSort = (arr) => {
-  for (let j = 1; j <= arr.length - 1; j++) { // j is arr index
-    let key = arr[j]
-    let i = j - 1
+  for (let j = 1; j <= arr.length - 1; j++) {
+    // j is arr index
+    let key = arr[j];
+    let i = j - 1;
     while (i >= 0 && arr[i] > key) {
-      arr[i + 1] = arr[i]
-      i -= 1
+      arr[i + 1] = arr[i];
+      i -= 1;
     }
-    arr[i + 1] = key
+    arr[i + 1] = key;
   }
 
-  return arr
-}
+  return arr;
+};
 
-console.log(insertionSort([14, -4, 17, 6, 22, 1, -5]))
+// console.log(insertionSort([14, -4, 17, 6, 22, 1, -5]));
 //             i    j
 //                  start from here
 
 // Merge Sort
+const merge = (leftNums, rightNums) => {
+  let l = 0;
+  let r = 0;
+  const output = [];
 
-const merge = (left, right) => {
-  const result = [];
-  let leffIndex = 0;
-  let rightIndex = 0;
+  while (l < leftNums.length && r < rightNums.length) {
+    output.push(leftNums[l] >= rightNums[r] ? rightNums[r++] : leftNums[l++]);
+  }
 
-  // while(leffIndex)
+  while (l < leftNums.length) output.push(leftNums[l++]);
+  while (r < rightNums.length) output.push(rightNums[r++]);
+
+  return output;
 };
 
 const mergeSort = (nums) => {
-  if (array.length === 1) {
-    return arry;
-  }
+  if (nums.length <= 1) return nums; // base case: 長度為1不用排序
 
-  const length = array.length;
-  const middle = Math.floor(length / 2);
-  const left = arrary.slice(0, middle);
-  const right = array.slice(middle);
+  const mid = Math.floor(nums.length / 2);
+  const left = mergeSort(nums.slice(0, mid)); // 左半邊排序
+  const right = mergeSort(nums.slice(mid)); // 右半邊排序
 
-  return merge(mergeSort(left), mergeSort(right));
+  return merge(left, right); // 合併左右兩邊
 };
 
 // console.log(mergeSort(numbers));
